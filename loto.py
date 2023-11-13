9d834451c248358900355462adc80c8de828caea
inishevv <146018080+inishevv@users.noreply.github.com>
2023-11-13 21:30:54 +0500

    Ввод количества боченков, проверка на число

--- loto.py
+++ loto.py
@@ -1,4 +1,17 @@
 import logging
 
-logging.basicConfig(level=logging.INFO, filename="py_log.log",filemode="w")
+logging.basicConfig(level=logging.INFO, filename="py_log.log",filemode="a")
 logging.warning("Create")
+
+def chislo(prompt):
+    while True:
+        user_n = input(prompt)
+        try:
+            number = float(user_n)
+            return number
+        except ValueError:
+            logging.warning("Введено не число, попробуйте снова.")
+
+
+logging.warning('Введите количество боченков: ')
+n = chislo('')
