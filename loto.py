c14c40657a11587a5805ba2d77d3586394703d99
inishevv <146018080+inishevv@users.noreply.github.com>
2023-11-13 21:54:56 +0500

    выдача рандомной последовательности чисел от 1 до n

--- loto.py
+++ loto.py
@@ -1,4 +1,5 @@
 import logging
+from random import choice
 
 logging.basicConfig(level=logging.INFO, filename="py_log.log",filemode="a")
 logging.warning("Create")
@@ -7,11 +8,27 @@ def chislo(prompt):
     while True:
         user_n = input(prompt)
         try:
-            number = float(user_n)
+            number = int(user_n)
             return number
         except ValueError:
+            print("Введено не число, попробуйте снова.")
             logging.warning("Введено не число, попробуйте снова.")
 
-
+print('Введите количество боченков: ')
 logging.warning('Введите количество боченков: ')
 n = chislo('')
+logging.warning(n)
+
+m = []
+for i in range(1, n + 1):
+    m.append(i)
+
+itog = ''
+while m:
+    random_item = choice(m)
+    m.remove(random_item)
+    itog += str(random_item)
+    itog += ' '
+print(itog)
+logging.warning(itog)
+    
