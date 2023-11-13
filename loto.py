d9c3107b956a3aa70ce80ddb7d20ab51576e8eee
inishevv <146018080+inishevv@users.noreply.github.com>
2023-11-13 22:04:57 +0500

    добавлено описание функций и блоков программы

--- loto.py
+++ loto.py
@@ -1,9 +1,13 @@
+#для вызов библиотек
 import logging
 from random import choice
 
+#для записи работы программы в log-файл
 logging.basicConfig(level=logging.INFO, filename="py_log.log",filemode="a")
-logging.warning("Create")
+#для обозначения старта программы в log-файле
+logging.warning('Start')
 
+#для проверки введенного значения N на число
 def chislo(prompt):
     while True:
         user_n = input(prompt)
@@ -14,21 +18,23 @@ def chislo(prompt):
             print("Введено не число, попробуйте снова.")
             logging.warning("Введено не число, попробуйте снова.")
 
+#для ввода значения N 
 print('Введите количество боченков: ')
 logging.warning('Введите количество боченков: ')
 n = chislo('')
 logging.warning(n)
 
+#для создания массива значений боченков - "мешок с боченками"
 m = []
 for i in range(1, n + 1):
     m.append(i)
 
 itog = ''
 while m:
-    random_item = choice(m)
-    m.remove(random_item)
-    itog += str(random_item)
+    random_item = choice(m) #для вытаскивания рандомного боченка
+    m.remove(random_item) #для удаления боченка из мешка
+    itog += str(random_item) #для добавления боченка в итоговую последовательность
     itog += ' '
-print(itog)
+print('Последовательность вытягивания боченков: ' + itog)
 logging.warning(itog)
     
